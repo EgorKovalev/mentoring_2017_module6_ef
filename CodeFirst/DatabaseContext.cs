17369565c9d699969f5512270d1c044a18154795
Yahor_Kavaliou <yahor.kavaliou@thomsonreuters.com>
2017-05-23 20:39:04 +0300

    initial commit

---
+++ CodeFirst/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace CodeFirst
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Item> Items { get; set; }
+		public DbSet<Project> Projects { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	public enum Role
+	{
+		Developer = 1,
+		QA,
+		Manager
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public virtual ICollection<Item> Items { get; set; }
+		public virtual ICollection<Project> Projects { get; set; }
+		public virtual Role Role { get; set; }
+	}
+}
